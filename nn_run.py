d159161d5c991d7cb95a3ba501bb5095db2ca7b8
Nikunj Jain <nikunj.jain@berkeley.edu>
2018-03-14 19:03:31 -0700

    Added some changes

--- nn_run.py
+++ nn_run.py
@@ -33,7 +33,7 @@ def neural_net_run(m, k_sq, learning_rate, epochs, batch_size, x_stddev,
   l1 = tf.layers.dense(
     x0, num_units_1, activation=activation_fn_1, use_bias=True)
   l2 = tf.layers.dense(
-    l1, m, activation=activation_fn_1, use_bias=True)
+    l1, m, activation=tf.identity, use_bias=True)
 
   u1 = l2
   u1_cost = (tf.norm(u1)**2) / batch_size
@@ -45,7 +45,7 @@ def neural_net_run(m, k_sq, learning_rate, epochs, batch_size, x_stddev,
   l3 = tf.layers.dense(
     y1, num_units_2, activation=activation_fn_2, use_bias=True)
   l4 = tf.layers.dense(
-    l3, m, activation=activation_fn_2, use_bias=True)
+    l3, m, activation=tf.identity, use_bias=True)
 
   u2 = -l4
   x2 = x1 + u2
@@ -119,35 +119,35 @@ def neural_net_run(m, k_sq, learning_rate, epochs, batch_size, x_stddev,
       print('Mean loss is {}'.format(np.sum(wits_cost_test)))
 
       #PLOTTING. Unnecessary for now because we're just doing hyperparameter search
-      # l1, = plt.plot(x0_test, u1_test[0], label="U1 Test")
-      # plt.legend(handles=[l1])
-      # plt.title("{} Unit NN With Activation Fn {}".format(
-      #   str(num_units_1), str(activation_fn_1)))
-      # plt.savefig("figs/{}_{}_{}_u_1_{}.png".format(
-      #   str(learning_rate), str(num_units_1), str(num_units_2) ,str(k_sq)))
+      l1, = plt.plot(x0_test, u1_test[0], label="U1 Test")
+      plt.legend(handles=[l1])
+      plt.title("{} Unit NN With Activation Fn {}".format(
+        str(num_units_1), str(activation_fn_1)))
+      plt.savefig("figs/{}_{}_{}_u_1_{}.png".format(
+        str(learning_rate), str(num_units_1), str(num_units_2) ,str(k_sq)))
 
-      # plt.clf()
-      # l1, = plt.plot(y1_test, u2_test[0], label="U2 Test")
-      # plt.legend(handles=[l1])
-      # plt.title("{} Unit NN With Activation Fn {}".format(
-      #   str(num_units_2), str(activation_fn_2)))
-      # plt.savefig("figs/{}_{}_{}_u_2_{}.png".format(
-      #   str(learning_rate), str(num_units_1), str(num_units_2) ,str(k_sq)))
+      plt.clf()
+      l1, = plt.plot(y1_test, u2_test[0], label="U2 Test")
+      plt.legend(handles=[l1])
+      plt.title("{} Unit NN With Activation Fn {}".format(
+        str(num_units_2), str(activation_fn_2)))
+      plt.savefig("figs/{}_{}_{}_u_2_{}.png".format(
+        str(learning_rate), str(num_units_1), str(num_units_2) ,str(k_sq)))
 
-      # plt.clf()
-      # l2, = plt.plot(x0_test, x1_test[0], label="X1 Test")
-      # plt.title("{} Unit NN With Activation Fn {}".format(
-      #   str(num_units_1), str(activation_fn_1)))
-      # plt.legend(handles=[l2])
-      # plt.savefig("figs/{}_{}_{}_x_1_{}.png".format(
-      #   str(learning_rate), str(num_units_1), str(num_units_2) ,str(k_sq)))
+      plt.clf()
+      l2, = plt.plot(x0_test, x1_test[0], label="X1 Test")
+      plt.title("{} Unit NN With Activation Fn {}".format(
+        str(num_units_1), str(activation_fn_1)))
+      plt.legend(handles=[l2])
+      plt.savefig("figs/{}_{}_{}_x_1_{}.png".format(
+        str(learning_rate), str(num_units_1), str(num_units_2) ,str(k_sq)))
 
 if __name__ == "__main__":
   #learning_rates = [0.01, 0.001, 0.0001, 0.005]
   learning_rates = [5e-5]
-  num_units_1s = [100, 150]
+  num_units_1s = [150]
   num_units_2s = [30]
-  optimizers = [tf.train.AdamOptimizer, tf.train.RMSPropOptimizer, tf.train.GradientDescentOptimizer]
+  optimizers = [tf.train.AdamOptimizer]
   activation_fn_1 = tf.nn.sigmoid
   activation_fn_2 = tf.nn.sigmoid
 
@@ -173,8 +173,8 @@ if __name__ == "__main__":
             print('k_squared: {}'.format(k_squared))
             print('Number of epochs: {}'.format(num_epochs))
             print('-----------------------------------------------\n')
-            neural_net_run(m = 1, k_sq = k_squared, learning_rate = lr, epochs = num_epochs, batch_size = 100, 
-              x_stddev = 3, activation_fn_1 = activation_fn_1, activation_fn_2 = activation_fn_2, num_units_1 = n_units_1, 
-              num_units_2 = n_units_2, decay = decay_rate, test_averaging = 100, optimizer_func = optimizer)
+            neural_net_run(m = 1, k_sq = k_squared, learning_rate = lr, epochs = num_epochs, batch_size = 1000, 
+              x_stddev = 5, activation_fn_1 = activation_fn_1, activation_fn_2 = activation_fn_2, num_units_1 = n_units_1, 
+              num_units_2 = n_units_2, decay = decay_rate, test_averaging = 10000, optimizer_func = optimizer)
             print('-----------------------------------------------\n')
             run_num += 1
